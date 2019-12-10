932bea15d00aebb4be6842c6bd4ef75cd2d94948
Kevin Seibel <seibel.kevin@icloud.com>
2019-12-10 20:08:29 +0100

    Initial Commit

---
+++ Taschenrechner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Taschenrechner
+//
+//  Created by Kevin Seibel on 10.12.19.
+//  Copyright © 2019 Kevin Seibel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
